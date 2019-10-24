35a5c8cb0f1cff310fb3ccda2a12b06f55060278
Valentin Fadloun <v.fadloun@gmail.com>
2019-10-24 16:01:15 +0200

    Ajout des commentaires :pig:

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,23 @@ var express = require('express');
 var router = express.Router();
 var User = require("../schemas/user");
 
-/* GET users listing. */
+/**
+ * Route that return all the users presents in the database
+ */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({ coucou: "shops" });
 });
 
+
+/**
+ * Route that add the user in the database
+ * Data expected in the body :
+ * {
+ *  firstName: First Name of the user,
+ *  lastName: Last Name of the user,
+ *  mail: Email of the user,
+ *  status: Status of the user ()}
+ */
 router.post("/add", function(req, res, next) {
   const { firstName, lastName, mail, status } = req.body;
   const newUser = new User({ firstName, lastName, mail, status });
